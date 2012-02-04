37e7b72f24789d3467effdb6ee3a88c2022ddc50
kpj <kpjkpjkpjkpjkpjkpj@googlemail.com>
2012-02-04 15:57:57 +0100

    compileable

---
+++ Char.java
@@ -0,0 +1,31 @@
+import java.awt.*;
+
+public class Char
+{
+    String name;
+    Image world_image;
+    float x_pos, y_pos, height, width;
+    key_set set;
+    
+    public Char(String n, float x, float y, String w_i, key_set set)
+    {
+        name = n;
+        world_image = Toolkit.getDefaultToolkit().getImage(w_i);
+        height = world_image.getHeight(null);
+        width = world_image.getWidth(null);
+        x_pos = x;
+        y_pos = y;
+        set = set;
+    }
+    
+    public Image get_image() {
+        return world_image;
+    }
+    
+    public float get_x() {
+        return x_pos;
+    }
+    public float get_y() {
+        return y_pos;
+    }
+}
---
+++ key_set.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class key_set
+{
+    HashMap<String, HashMap> player_set = new HashMap<String, HashMap>();
+
+    public key_set()
+    {
+        String name = "Hinz";
+        
+        HashMap<String, Character> action2key = new HashMap<String, Character>();
+        
+        action2key.put("UP", 'w');
+        action2key.put("LEFT", 'a');
+        action2key.put("RIGHT", 'd');
+        action2key.put("DOWN", 's');
+
+        player_set.put(name, action2key);
+    }
+}
